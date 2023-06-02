2ddcfb532f7e0d56da81a0539c4cedb9d219fa85
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-06-02 20:01:43 -0300

    Adding FindUsersUseCase test

--- src/main/java/com/iCritic/iCritic/core/usecase/FindUsersUseCase.java
+++ src/main/java/com/iCritic/iCritic/core/usecase/FindUsersUseCase.java
@@ -2,7 +2,6 @@ package com.iCritic.iCritic.core.usecase;
 
 import com.iCritic.iCritic.core.model.User;
 import com.iCritic.iCritic.core.usecase.boundary.FindUsersBoundary;
-import com.iCritic.iCritic.entrypoint.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
