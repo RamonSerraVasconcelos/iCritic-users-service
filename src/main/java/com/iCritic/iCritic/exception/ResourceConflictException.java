5cd456593979cb4ce2cec018cfb040232b6fe434
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-06-01 23:47:00 -0300

    Adding Global Exception handler

--- src/main/java/com/iCritic/iCritic/exception/ResourceConflictException.java
+++ src/main/java/com/iCritic/iCritic/exception/ResourceConflictException.java
@@ -1,9 +1,5 @@
 package com.iCritic.iCritic.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class ResourceConflictException extends RuntimeException {
     public ResourceConflictException(String message) {
         super(message);
---
+++ src/main/java/com/iCritic/iCritic/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.iCritic.iCritic.exception.handler;
+
+import com.iCritic.iCritic.exception.ResourceConflictException;
+import com.iCritic.iCritic.exception.ResourceNotFoundException;
+import com.iCritic.iCritic.exception.ResourceViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ErrorDetails buildResponseError(String message) {
+        return ErrorDetails.builder()
+                .message(message)
+                .build();
+    }
+
+    @ExceptionHandler(ResourceConflictException.class)
+    public ResponseEntity<ErrorDetails> resourceConflictExceptionHandler(ResourceConflictException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(buildResponseError(ex.getMessage()));
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildResponseError(ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceViolationException.class)
+    public ResponseEntity<ErrorDetails> resourceViolationExceptionHandler(ResourceViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(buildResponseError(ex.getMessage()));
+    }
+}
