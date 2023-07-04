1a26b92433b4b44e47da42c9edf3bedecb8e3c77
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-07-03 23:30:23 -0300

    Adding SaveImageInfoGateway test

--- src/test/java/com/iCritic/users/dataprovider/gateway/azure/impl/UploadImageAzureGatewayTest.java
+++ src/test/java/com/iCritic/users/dataprovider/gateway/azure/impl/UploadImageAzureGatewayTest.java
@@ -1,26 +1,23 @@
-package com.iCritic.users.dataprovider.azure.impl;
+package com.iCritic.users.dataprovider.gateway.azure.impl;
 
-import com.azure.storage.blob.BlobClient;
-import com.azure.storage.blob.BlobClientBuilder;
-import com.azure.storage.blob.BlobContainerClientBuilder;
-import com.iCritic.users.config.AzureStorageConfig;
-import com.iCritic.users.core.fixture.ImageFixture;
-import com.iCritic.users.core.model.Image;
-import com.iCritic.users.dataprovider.gateway.azure.impl.UploadImageAzureGateway;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Blob;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import com.azure.storage.blob.BlobClient;
+import com.iCritic.users.config.AzureStorageConfig;
+import com.iCritic.users.core.fixture.ImageFixture;
+import com.iCritic.users.core.model.Image;
 
 @ExtendWith(MockitoExtension.class)
 public class UploadImageAzureGatewayTest {
