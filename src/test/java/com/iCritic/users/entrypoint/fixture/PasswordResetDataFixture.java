11a00cf16f4d830db0e32a84a20bf7465f35875b
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-08-12 15:11:51 -0300

    Adding tests for auth resource

---
+++ src/test/java/com/iCritic/users/entrypoint/fixture/PasswordResetDataFixture.java
@@ -0,0 +1,14 @@
+package com.iCritic.users.entrypoint.fixture;
+
+import com.iCritic.users.entrypoint.model.PasswordResetData;
+
+public class PasswordResetDataFixture {
+
+    public static PasswordResetData load() {
+        return PasswordResetData.builder()
+                .email("test@test.test")
+                .passwordResetHash("test")
+                .password("12345678")
+                .build();
+    }
+}
